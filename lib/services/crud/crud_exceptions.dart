1e98fa7a3ccec315ee07b0b16752daa09faffd0c
Pranay Buradkar <pranayyb@Pranays-Air.local>
2024-10-23 00:50:31 +0530

    crud added

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,45 @@
+
+class DatabaseAlreadyOpenException implements Exception {
+  @override
+  String toString() => "Database is already open";
+}
+
+class UnableToGetDocumentsDirectory implements Exception {
+  @override
+  String toString() => "Unable to get documents directory";
+}
+
+class DatabaseIsNotOpen implements Exception {
+  @override
+  String toString() => "Database is not open";
+}
+
+class CouldNotDeleteUser implements Exception {
+  @override
+  String toString() => "Could not delete user";
+}
+
+class UserAlreadyExists implements Exception {
+  @override
+  String toString() => "User already exists";
+}
+
+class CouldNotFindUser implements Exception {
+  @override
+  String toString() => "Could not find user";
+}
+
+class CouldNotDeleteNote implements Exception {
+  @override
+  String toString() => "Could not delete note";
+}
+
+class CouldNotFindNote implements Exception {
+  @override
+  String toString() => "Could not find note";
+}
+
+class CouldNotUpdateNote implements Exception {
+  @override
+  String toString() => "Could not update note";
+}
